0bcc6056ed19b24684f1aac8304dae9237d1312e
MariaLauraElias <eliasmarialaura@gmail.com>
2023-09-26 17:21:03 -0300

    agregué las funcionalidades de products al archivo types.js

--- src/types/types.js
+++ src/types/types.js
@@ -3,5 +3,11 @@ export const types = {
         login:   '[AUTH] - LOGIN',
         logout:  '[AUTH] - LOGOUT',
         registerUser: '[AUTH] - REGISTERUSER'
+    },
+    products: {
+        listProducts: '[PRODUCTS] - LISTPRODUCTS',
+        addProduct: '[PRODUCTS] - ADDPRODUCT',
+        editProduct: '[PRODUCTS] - EDITPRODUCT',
+        deleteProduct: '[PRODUCTS] - DELETEPRODUCT',
     }
 }
