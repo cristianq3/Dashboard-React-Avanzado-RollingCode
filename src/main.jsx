7bb451798673599ee5c7263da48d4bac031cba65
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-10-04 18:25:25 -0300

    correcciones en main: traemos todos los providers

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./providers/AuthProvider.jsx";
-import { UsersProvider } from "./providers/UsersProvider.jsx";
 import { ProductProvider } from "./providers/ProductProvider.jsx";
+import { UsersProvider } from "./providers/UsersProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
