d3f928a4c446947b83afdb7a5c11f2efbebdb3ea
Cristian Quiroga <cristianq3@gmail.com>
2023-11-27 00:27:20 -0300

    filtro

--- src/layouts/dashboard/nav/config.jsx
+++ src/layouts/dashboard/nav/config.jsx
@@ -31,21 +31,21 @@ const navConfig = [
     path: "/dashboard/sales",
     icon: icon("ic_dollar"),
   },
-  {
-    title: "blog",
-    path: "/dashboard/blog",
-    icon: icon("ic_blog"),
-  },
-  {
-    title: "login",
-    path: "/login",
-    icon: icon("ic_lock"),
-  },
-  {
-    title: "Not found",
-    path: "/404",
-    icon: icon("ic_disabled"),
-  },
+  // {
+  //   title: "blog",
+  //   path: "/dashboard/blog",
+  //   icon: icon("ic_blog"),
+  // },
+  // {
+  //   title: "login",
+  //   path: "/login",
+  //   icon: icon("ic_lock"),
+  // },
+  // {
+  //   title: "Not found",
+  //   path: "/404",
+  //   icon: icon("ic_disabled"),
+  // },
 ];
 
 export default navConfig;
